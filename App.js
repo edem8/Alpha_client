528dce2038bed44fab05cef71cd64fa27e28d305
edem8 <eddieblack159@gmail.com>
2023-01-26 19:55:45 +0000

    homescreen set up

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image} from 'react-native';
+import fit from './assets/images/fit.jpg'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+
+      <Image source={fit} style={styles.fit_image}/>
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
