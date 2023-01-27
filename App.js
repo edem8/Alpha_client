cf08db0a2088786383e312ee6bf8cc547b7cba9b
edem8 <eddieblack159@gmail.com>
2023-01-26 16:18:23 -0800

    Homescreen

--- App.js
+++ App.js
@@ -1,15 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Image} from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Image, TextInput} from 'react-native';
 import fit from './assets/images/fit.jpg'
 
 export default function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <SafeAreaView style={styles.container}>
 
       <Image source={fit} style={styles.fit_image}/>
-      <Text style={styles.fit_image_text}> Believe In yourself</Text>
-
+     
       <StatusBar style="auto" />
+
+      <View style={styles.LogIns_container}>
+
+        <View style={styles.email_container}>
+        <Text>Username Or Email</Text>
+        <TextInput
+        placeholder='Username or Email'
+        placeholderTextColor='grey'
+        onChangeText={(email) => setEmail(email)}
+        style={styles.textInput}/>
+        </View>
+
+        <View style={styles.password_container}>
+        <Text>Password</Text>
+        <TextInput
+        placeholder='Password'
+        placeholderTextColor='grey'
+        onChangeText={(password) => setPassword(password)}
+        style={styles.textInput}/>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
@@ -19,16 +42,33 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+
   },
   
   fit_image: {
     width: 375,
-    height: 250,
+    marginTop: 10,
+    height: 270,
+    flex: 2
   },
 
-  fit_image_text: {
-    position: 'absolute',
-    color: 'white'
+  LogIns_container: {
+    flex: 3
+  },
+
+  email_container: {
+    marginVertical: 20
+  },
+
+  textInput: {
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: 'grey',
+    fontSize: 16,
+    paddingHorizontal: 100,
+    paddingVertical: 8,
+    textAlign: 'center',
+    
+
   }
 });
